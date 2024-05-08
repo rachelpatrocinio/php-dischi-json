fa04fd0d9df2998e9c8655585bf7433657e4d5ef
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-05-08 15:49:09 +0200

    Added: style on card

--- index.php
+++ index.php
@@ -11,13 +11,20 @@
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 
     <div id="app">
-        <ul>
-            <li v-for="(disk,i) in disks">
-                <h2>{{disk.title}}</h2>
-                <h5>{{disk.author}}</h5>
-                <h6>{{disk.year}}</h6>
-            </li>
-        </ul>
+        <main>
+            <div class="container">
+                <div class="row d-flex flex-wrap">
+                    <div class="card col-4 p-5" v-for="(disk,i) in disks" :key="i">
+                        <img :src="disk.poster" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">{{disk.title}}</h5>
+                            <p class="card-text">{{disk.author}}</p>
+                            <p class="card-text">{{disk.year}}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
     </div>
 
     <script src="./js/app.js"></script>
