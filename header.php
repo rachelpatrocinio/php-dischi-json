5eb938db36788ed2f7a6523c66de98b9471ce159
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-05-08 16:09:05 +0200

    Added: header.php and included it on index.php

---
+++ header.php
@@ -0,0 +1,7 @@
+<header class="main-header d-flex align-items-center">
+    <div class="container">
+        <div class="row">
+            <h3 class="text-white">SPOTIFY</h3>
+        </div>
+    </div>
+</header>
--- index.php
+++ index.php
@@ -5,14 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="./css/style.css">
     <title>php-dischi-json</title>
 </head>
 <body>
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 
     <div id="app">
-        <main>
+        <?php include __DIR__ .'/header.php'?>
+        <main class="mt-5 mb-5">
             <div class="container">
                 <div class="row d-flex flex-wrap">
                     <div class="col-4" v-for="(disk,i) in disks" :key="i">
